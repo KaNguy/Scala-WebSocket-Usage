66de0597048ecfa26795ae210311379f2f20c354
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-06-14 14:40:19 -0700

    Addition: ScalaWebSocket.scala for the websocket

---
+++ src/main/scala/scalaWebSocket/ScalaWebSocket.scala
@@ -0,0 +1,17 @@
+package scalaWebSocket
+
+/**
+ * Created by KaNguy - 06/14/2021
+ * File scalaWebSocket.ScalaWebSocket.scala
+ */
+
+import java.net.http.{HttpClient, HttpHeaders, HttpRequest, HttpResponse, WebSocket}
+import java.net.http.WebSocket.{Builder, Listener}
+import java.net.URI
+import java.nio.ByteBuffer
+import java.util.concurrent.CompletionStage
+import java.util.EventListener
+
+class ScalaWebSocket {
+
+}
