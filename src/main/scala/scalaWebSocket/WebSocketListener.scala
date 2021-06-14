aefea67b185a22bd87e2c2d1f593f3f1dc4cc002
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-06-14 14:36:14 -0700

    Fix: Add CompletionStage for events

--- src/main/scala/scalaWebSocket/WebSocketListener.scala
+++ src/main/scala/scalaWebSocket/WebSocketListener.scala
@@ -8,6 +8,7 @@ package scalaWebSocket
 import java.net.http.{HttpClient, HttpHeaders, HttpRequest, HttpResponse, WebSocket}
 import java.net.http.WebSocket.{Listener, Builder}
 import java.net.URI
+import java.util.concurrent.CompletionStage
 
 case class WebSocketListener() extends Listener {
   override def onOpen(webSocket: WebSocket): Unit = {
