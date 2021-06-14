bbe8bfb5997def9f8ed7aa1f17bf79d01b077aa6
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-06-14 14:31:19 -0700

    Feature: WebSocketListener

---
+++ src/main/scala/scalaWebSocket/WebSocketListener.scala
@@ -0,0 +1,14 @@
+package scalaWebSocket
+
+/**
+ * Created by KaNguy - 06/14/2021
+ * File scalaWebSocket.WebSocketListener.scala
+ */
+
+import java.net.http.{HttpClient, HttpHeaders, HttpRequest, HttpResponse, WebSocket}
+import java.net.http.WebSocket.{Listener, Builder}
+import java.net.URI
+
+case class WebSocketListener() extends Listener {
+
+}
