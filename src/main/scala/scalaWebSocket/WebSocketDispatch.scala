11661f99421d3cd63c07fe74c2fce945ad4a5832
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-06-15 09:51:49 -0700

    Feature: onText to the trait

--- src/main/scala/scalaWebSocket/WebSocketDispatch.scala
+++ src/main/scala/scalaWebSocket/WebSocketDispatch.scala
@@ -9,8 +9,12 @@ package scalaWebSocket
 import java.net.http.WebSocket
 
 // Utilities
+import java.util.concurrent.CompletionStage
+
+// Utilities
 import java.util.EventListener
 
 trait WebSocketDispatch extends EventListener {
   def onOpen(webSocket: WebSocket): Unit
+  def onText(webSocket: WebSocket, data: CharSequence, last: Boolean): CompletionStage[_]
 }
