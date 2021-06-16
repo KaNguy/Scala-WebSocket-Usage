6b787b53cc175fcdcf1d8515574da0f228e0db52
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-06-16 16:57:12 -0700

    Tests: New tests

---
+++ src/test/scala/WebSocketTest.scala
@@ -0,0 +1,28 @@
+import scalaWebSocket.ScalaWebSocket
+
+import java.net.http.WebSocket
+import java.net.http.WebSocket.Listener
+import java.util.concurrent.CompletionStage
+
+object WebSocketTest extends App {
+  val listener: Listener = new Listener {
+    override def onOpen(webSocket: WebSocket): Unit = {
+      println("WebSocket connection opened")
+      super.onOpen(webSocket)
+    }
+
+    override def onText(webSocket: WebSocket, data: CharSequence, last: Boolean): CompletionStage[_] = {
+      println("Data: " + data)
+      super.onText(webSocket, data, last)
+    }
+
+    override def onClose(webSocket: WebSocket, statusCode: Int, reason: String): CompletionStage[_] = {
+      println("Connection closed: " + statusCode + ", " + reason)
+      super.onClose(webSocket, statusCode, reason)
+    }
+  }
+
+  val ws = new ScalaWebSocket("wss://echo.websocket.org", listener)
+  ws.send("Message", true)
+  ws.close(1000, "None")
+}
