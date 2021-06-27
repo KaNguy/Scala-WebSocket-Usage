a29684b9a24f0427aa0e725b443ab76fd6e527b2
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-06-26 22:19:49 -0700

    Experiment: Make an interactions method for general WebSocket interactions—hasn't been tested

--- src/main/scala/scalaWebSocket/ScalaWebSocket.scala
+++ src/main/scala/scalaWebSocket/ScalaWebSocket.scala
@@ -12,6 +12,7 @@ import java.net.{ConnectException, URI}
 
 // New I/O
 import java.nio.ByteBuffer
+import java.nio.CharBuffer
 
 // Utilities
 import java.util.concurrent.{CompletableFuture, CompletionStage, CountDownLatch, TimeUnit}
@@ -27,6 +28,25 @@ class ScalaWebSocket(var url: String = null, var listener: Listener = new WebSoc
 
   private val httpClient: HttpClient = HttpClient.newHttpClient()
   private var webSocket: WebSocket = httpClient.newWebSocketBuilder().buildAsync(URI.create(url), listener).join()
+
+  def interact(action: String = null, data: CharSequence = null, message: ByteBuffer = null, statusCode: Int = WebSocket.NORMAL_CLOSURE, reason: String = "", last: Boolean = false, timeout: Int = 1000): Unit = {
+    action.toUpperCase match {
+      case "SEND" => {
+        this.webSocket.sendText(data, last)
+      } case "CLOSE" => {
+        this.webSocket.sendClose(statusCode, reason)
+      } case "PING" => {
+        this.webSocket.sendPing(message)
+      } case "PONG" => {
+        this.webSocket.sendPong(message)
+      } case "BINARY" => {
+        // Sends binaries, untested
+        this.webSocket.sendBinary(ByteBuffer.allocate(CharBuffer.wrap(data).length() * Character.BYTES), last)
+      }
+      case _ => ()
+    }
+    latch.await(timeout, TimeUnit.MILLISECONDS)
+  }
 
   def send(data: CharSequence, last: Boolean, timeout: Int = 1000): Unit = {
     try {
